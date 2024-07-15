528600c5ae1d913f7fd6ccb2b484d5c93f6cec8d
Hori <0513simon2001@gmail.com>
2024-07-15 21:44:28 +0900

    feat: Add dynamic status icon and participant list to HostPage
    This commit modifies the HostPage widget to include a dynamic status icon and a list of participants. The HostPage widget is now a StatefulWidget to enable state changes. The status icon toggles between a check mark and an error icon when the "締め切り" button is pressed. The list of participants is displayed using a ListView.builder and each participant is represented by a ListTile.
    
    The changes also include updating the onPressed event handler for the "開催者ページ" button in the MyHomePage widget to navigate to the HostPage with the conference name and a list of participants.
    
    This commit addresses the need to display the status and participants in the HostPage and improves the user experience by providing dynamic functionality.

--- lib/HostPage/main.dart
+++ lib/HostPage/main.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
-class HostPage extends StatelessWidget {
-  const HostPage({Key? key}) : super(key: key);
+class HostPage extends StatefulWidget {
+  final String conferenceName;
+  final List<String> participants;
+
+  const HostPage({super.key, required this.conferenceName, required this.participants});
+
+  @override
+  _HostPageState createState() => _HostPageState();
+}
+
+class _HostPageState extends State<HostPage> {
+  IconData statusIcon = Icons.check_circle;
+
+  void _changeStatusIcon() {
+    setState(() {
+      if (statusIcon == Icons.check_circle) {
+        statusIcon = Icons.error;
+      } else {
+        statusIcon = Icons.check_circle;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: const Text('開催者ページ'),
+        title: Text(widget.conferenceName),
       ),
-      body: const Center(
-        child: Text('開催者ページ'),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.participants.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(statusIcon),
+                  title: Text(widget.participants[index]),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: _changeStatusIcon,
+              child: const Text('締め切り'),
+            ),
+          ),
+        ],
       ),
     );
   }
