19ae4a7108c6435370d22e10a4203faf39d6d063
Hori <0513simon2001@gmail.com>
2024-07-17 16:38:53 +0900

    fix commit

--- lib/HostPage/main.dart
+++ lib/HostPage/main.dart
@@ -7,17 +7,14 @@ import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
 
 class HostPage extends StatefulWidget {
-  final DeviceType deviceType;
   final String conferenceName;
   final List<String> participants;
 
-  const HostPage({super.key, required this.deviceType, required this.conferenceName, required this.participants});
+  const HostPage({super.key, required this.conferenceName, required this.participants});
 
   @override
   _HostPageState createState() => _HostPageState();
 }
-
-enum DeviceType { advertiser, browser }
 
 class _HostPageState extends State<HostPage> {
   IconData statusIcon = Icons.check_circle;
@@ -28,7 +25,7 @@ class _HostPageState extends State<HostPage> {
   late StreamSubscription subscription;
   late StreamSubscription receivedDataSubscription;
 
-  void _changeStatusIcon() {
+  void _changeStatusIcon(index) {
     setState(() {
       if (statusIcon == Icons.highlight_off) {
         statusIcon = Icons.check_circle_outline;
@@ -40,6 +37,33 @@ class _HostPageState extends State<HostPage> {
     });
   }
 
+  void _registerAttendance(name,deviceId ) {
+    // 出席登録の処理
+    // nameのstringがconferenceNameの中に含まれているか確認してインデックスを抽出
+    // participantsのインデックスを抽出して、そのインデックスのstatusIconを変更する
+      // nameがconferenceNameの中に含まれているか確認してインデックスを抽出
+    
+    // final myController = TextEditingController();
+    int index = widget.conferenceName.indexOf(name);
+    _changeStatusIcon(index);
+    // // 出席完了メッセージを送信
+    nearbyService.sendMessage(
+      deviceId,
+      "出席完了"
+    );
+    // // 接続を切る
+    nearbyService.disconnectPeer(deviceID: deviceId);
+}
+
+void _aggregateAttendanceData(){
+  setState(() {
+    //出席情報集計処理
+    // bluetooth待ち解除
+    // nearbyService.stopAdvertisingPeer();
+    //集計結果画面遷移？？
+  });
+}
+
   @override
   void initState() {
     super.initState();
@@ -47,6 +71,11 @@ class _HostPageState extends State<HostPage> {
   }
   @override
   void dispose() {
+    subscription.cancel();
+    receivedDataSubscription.cancel();
+    nearbyService.stopBrowsingForPeers();
+    nearbyService.stopAdvertisingPeer();
+    super.dispose();
   }
 
   @override
@@ -72,7 +101,7 @@ class _HostPageState extends State<HostPage> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
-              onPressed: _changeStatusIcon,
+              onPressed: _aggregateAttendanceData,
               child: const Text('締め切り'),
             ),
           ),
@@ -98,17 +127,12 @@ class _HostPageState extends State<HostPage> {
         strategy: Strategy.P2P_STAR, // 1-to-N
         callback: (isRunning) async {
           if (isRunning) {
-            if (widget.deviceType == DeviceType.browser) {
-              await nearbyService.stopBrowsingForPeers();
-              await Future.delayed(Duration(microseconds: 200));
-              await nearbyService.startBrowsingForPeers();
-            } else {
               await nearbyService.stopAdvertisingPeer();
               await nearbyService.stopBrowsingForPeers();
               await Future.delayed(Duration(microseconds: 200));
               await nearbyService.startAdvertisingPeer();
               await nearbyService.startBrowsingForPeers();
-            }
+            // }
           }
         });
     subscription =
@@ -136,15 +160,12 @@ class _HostPageState extends State<HostPage> {
       });
     });
 
+    // メッセージ（氏名）を受け取った場合の処理
     receivedDataSubscription =
         nearbyService.dataReceivedSubscription(callback: (data) {
       print("dataReceivedSubscription: ${jsonEncode(data)}");
-      showToast(jsonEncode(data),
-          context: context,
-          axis: Axis.horizontal,
-          alignment: Alignment.center,
-          position: StyledToastPosition.bottom);
+      // data["message"]（氏名）を取り出して出席確認の処理に入る
+      _registerAttendance(data["message"],data["deviceId"]);
     });
   }
 }
-
