38b332f2632ff08aa87dff5b8da7ec0291a45632
dorrot101 <kimhyunoo101@gmail.com>
2024-07-20 14:52:41 +0900

    update: add material ui styles

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:attendance_app/HostPage/SelectPage.dart';
 import 'package:flutter/material.dart';
 import 'GuestPage/main.dart';
 import 'HostPage/main.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +26,36 @@ class MyApp extends StatelessWidget {
 
 enum DeviceType { advertiser, browser }
 
+class SquareButton extends StatelessWidget {
+  const SquareButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxSize = constraints.biggest.shortestSide * 0.7;
+        double size = maxSize > 300 ? 300 : maxSize;
+
+        return SizedBox(
+          width: size,
+          height: size,
+          child: ElevatedButton(
+            onPressed: () {
+              // 버튼 클릭 시 동작
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            child: const Text('Button'),
+          ),
+        );
+      },
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -33,26 +64,48 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const SelectPage())),
-              child: const Text('開催者ページ'),
-            ),
-            ElevatedButton(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(title),
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SelectPage())),
+                child: const Text('開催者ページ'),
+              ),
+              const SquareButton(),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(
+                      color: Colors.deepPurpleAccent, width: 4),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
                 onPressed: () => Navigator.push(context,
                     MaterialPageRoute(builder: (context) => const GuestPage())),
-                child: const Text('参加者ページ'))
-          ],
-        ),
-      ),
-    );
+
+                child: const Column(
+                  children: [
+                    Icon(LucideIcons.qrCode, size: 100),
+                    Text(
+                      '参加者ページ',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple),
+                    ),
+                  ],
+                ), // Text('参加者ページ'),
+              )
+            ])));
   }
 }
